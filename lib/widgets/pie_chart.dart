ab506750b6a09a7907746f723516dac09b5cc883
Vadym Pinchuk <vad.pinchuk@gmail.com>
2024-01-06 21:44:34 +0000

    Changed stats widget

---
+++ lib/widgets/pie_chart.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class PieChart extends StatelessWidget {
+  const PieChart({
+    super.key,
+    required this.data,
+    required this.labels,
+    required this.colors,
+    this.chartRadius = 50.0,
+  });
+
+  final List<double> data;
+  final List<String> labels;
+  final List<Color> colors;
+  final double chartRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CustomPaint(
+          painter: PieChartPainter(data, colors),
+          child: SizedBox(
+            width: chartRadius * 2,
+            height: chartRadius * 2,
+          ),
+        ),
+        const SizedBox(width: 16),
+        _buildLabels(context),
+      ],
+    );
+  }
+
+  Widget _buildLabels(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: labels.indexed.map((entry) {
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(Icons.circle, color: colors[entry.$1 % colors.length], size: 12),
+            const SizedBox(width: 4),
+            Text('${entry.$2} : ${data[entry.$1].toStringAsFixed(0)}'),
+          ],
+        );
+      }).toList(),
+    );
+  }
+}
+
+class PieChartPainter extends CustomPainter {
+  final List<double> data;
+  final List<Color> colors;
+
+  PieChartPainter(this.data, this.colors);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double total = data.fold(0, (sum, item) => sum + item);
+    double startAngle = -pi / 2;
+
+    final paint = Paint()..style = PaintingStyle.fill;
+
+    for (int i = 0; i < data.length; i++) {
+      final sweepAngle = data[i] / total * 2 * pi;
+      paint.color = colors[i % colors.length];
+      canvas.drawArc(
+        Rect.fromCenter(
+            center: Offset(size.width / 2, size.height / 2),
+            width: size.width,
+            height: size.height),
+        startAngle,
+        sweepAngle,
+        true,
+        paint,
+      );
+      startAngle += sweepAngle;
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/widgets/stats/yes_no_stats_widget.dart
+++ lib/widgets/stats/yes_no_stats_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:simple_survey/models/questions/survey_question.dart';
 import 'package:simple_survey/models/questions/yes_no_survey_question.dart';
+import 'package:simple_survey/widgets/pie_chart.dart';
 import 'package:simple_survey/widgets/stats/base_stats_widget.dart';
 
 class YesNoStatsWidget extends BaseStatsWidget<YesNoSurveyQuestion> {
@@ -17,27 +17,16 @@ class YesNoStatsWidget extends BaseStatsWidget<YesNoSurveyQuestion> {
 
   @override
   Widget childSpecificUI(BuildContext context, Map<String, dynamic> data) {
-    final positive = data[YesNoQuestionKey.yesValue];
-    final negative = data[YesNoQuestionKey.noValue];
-    final total = data[QuestionKey.numOfResponses];
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Stack(
         alignment: Alignment.center,
         children: <Widget>[
-          SizedBox(
-            width: size,
-            height: size,
-            child: CircularProgressIndicator(
-              value: positive / total,
-              strokeWidth: strokeWidth,
-              backgroundColor: Colors.grey.shade300,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
-          Text(
-            '${positive.toStringAsFixed(0)} / ${negative.toStringAsFixed(0)}',
-            style: const TextStyle(fontWeight: FontWeight.bold),
+          PieChart(
+            data: [data[YesNoQuestionKey.yesValue], data[YesNoQuestionKey.noValue]],
+            labels: const ['Yes', 'No'],
+            colors: const [Color(0xFF027DFD), Colors.deepOrange],
+            chartRadius: size / 2,
           ),
         ],
       ),
